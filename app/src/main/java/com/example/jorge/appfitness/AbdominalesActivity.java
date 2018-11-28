0e38c85348f6b89addf9511b2167992c3c91134b
joseline zevallos <joseline.zevallos@ucsp.edu.pe>
2018-11-28 12:53:45 -0500

    App

--- app/src/main/java/com/example/jorge/appfitness/AbdominalesActivity.java
+++ app/src/main/java/com/example/jorge/appfitness/AbdominalesActivity.java
@@ -10,7 +10,7 @@ public class AbdominalesActivity extends AppCompatActivity implements View.OnCli
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_abdominales);
 
 
     }
